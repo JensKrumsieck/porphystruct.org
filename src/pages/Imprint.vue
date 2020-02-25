cd1e1c289e3e730a2848e55f0b39dcb858f1aa5f
Jens Krumsieck <jenso666@live.de>
2020-02-25 18:38:43 +0100

    Update Imprint.vue

--- src/pages/Imprint.vue
+++ src/pages/Imprint.vue
@@ -22,21 +22,21 @@
             </p>
             <h4 class="title is-5 m-t-25">Disclaimer</h4>
             
-                <h5 class="title is-6">Accountability for content</h5>
-                <p>
-                    The contents of our pages have been created with the utmost care. However, we cannot guarantee the contents' accuracy, 
-                    completeness or topicality. According to statutory provisions, we are furthermore responsible for our own content on these web pages. 
-                    In this matter, please note that we are not obliged to monitor the transmitted or saved information of third parties, or investigate 
-                    circumstances pointing to illegal activity. Our obligations to remove or block the use of information under generally applicable laws remain 
-                    unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).
-                </p>
+            <h5 class="title is-6">Accountability for content</h5>
+            <p>
+                The contents of our pages have been created with the utmost care. However, we cannot guarantee the contents' accuracy, 
+                completeness or topicality. According to statutory provisions, we are furthermore responsible for our own content on these web pages. 
+                In this matter, please note that we are not obliged to monitor the transmitted or saved information of third parties, or investigate 
+                circumstances pointing to illegal activity. Our obligations to remove or block the use of information under generally applicable laws remain 
+                unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).
+            </p>
 
-                <h5 class="title is-6 m-t-25">Accountability for links</h5>
-                <p>
-                    Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of the linked pages. 
-                    No violations were evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective 
-                    link immediately.
-                </p>
+            <h5 class="title is-6 m-t-25">Accountability for links</h5>
+            <p>
+                Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of the linked pages. 
+                No violations were evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective 
+                link immediately.
+            </p>
 
             <h4 class="title is-5 m-t-25"> Copyright</h4>
             <p>
