387c2cfa497a4687a77d2306b81651d8cacb4883
Jens Krumsieck <jenso666@live.de>
2022-03-01 14:30:44 +0100

    static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { imagetools } from 'vite-imagetools';
 import preprocess from 'svelte-preprocess';
 
