7d237f1b855b976c3e08cba4fef21152aa4d13e7
Jens Krumsieck <jenso666@live.de>
2022-03-02 18:53:16 +0100

    fix admin

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -1,10 +1,9 @@
 <script>
 	import Container from '$lib/components/common/Container.svelte';
-	import Wrapper from '$lib/components/common/Wrapper.svelte';
 </script>
 
-<Container>
-	<Wrapper>
-		<iframe src="/cms/index.html" title="Admin" class="w-full h-screen" />
-	</Wrapper>
+<Container _class="fixed top-[7vh] w-full">
+	<div class="w-full h-[93vh] relative">
+		<iframe src="/cms/index.html" title="Admin" class="w-full h-full inset-0" />
+	</div>
 </Container>
