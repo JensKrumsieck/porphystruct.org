866e89522884d0ff988d4322a6b89031adeb6e6b
Dr. Jens Krumsieck <jenso666@live.de>
2024-03-25 14:36:02 +0100

    Update ContactJens.svelte

--- src/lib/components/ContactJens.svelte
+++ src/lib/components/ContactJens.svelte
@@ -5,15 +5,14 @@ import ContactCard from "./common/ContactCard.svelte";
 
 let img = `<img srcset=${jens} alt="Jens" loading="lazy" />`;
 let url = 'https://jenskrumsieck.de';
-let name = 'Jens Krumsieck, M. Sc.'
+let name = 'Dr. Jens Krumsieck'
 
 let researchgate = 'Jens-Krumsieck'
 let orcid = '0000-0001-6242-5846'
-let twitter = 'jens_ation'
 let instagram = 'jens.ation'
 let youtube = 'JensKrumsieck'
 let xing = 'Jens_Krumsieck'
 let github = 'jenskrumsieck'
-let linkedin = 'jens-krumsieck-849445211'
+let linkedin = 'jenskrumsieck'
 </script>
-<ContactCard {img} {url} {name} {researchgate} {orcid} {twitter} {instagram} {youtube} {xing} {github} {linkedin}/>
+<ContactCard {img} {url} {name} {researchgate} {orcid} {instagram} {youtube} {xing} {github} {linkedin}/>
